a399cfac7508dccdb20f2e2ec33185dcbadf9b34
Alpesh_Rajendra <alpeshrajendra@gmail.com>
2026-08-18 12:17:24 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source            = "./vpc"
+  source            = "./modules/vpc"
   vpc_cidr_block    = "192.168.0.0/16"
   subnet_cidr_block = "192.168.0.0/23"
   az                = "ap-southeast-1a"
@@ -9,9 +9,9 @@ module "vpc" {
 
 
 module "ec2" {
-  source            = "./ec2"
-  ami_id            = "ami-05f071c65e32875a8"
-  key_pair          = "pankaj"
+  source            = "./modules/ec2"
+  ami_id            = "YourAMI-ID"
+  key_pair          = "YourKeyName"
   inst_type         = "t3.micro"
   subnet_id         = module.vpc.subnet_id
   security_group_id = module.vpc.security_group_id
